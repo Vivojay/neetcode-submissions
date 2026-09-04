b01b1e64bc72d0f1dddb968df109f95682a7cdc3
Vivo Jay <67545205+Vivojay@users.noreply.github.com>
2026-09-04 15:02:37 +0530

    Add: anagram-groups - submission-19

---
+++ Data Structures & Algorithms/anagram-groups/submission-19.py
@@ -0,0 +1,16 @@
+import math
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # transform = lambda x: 2**(ord(x)-ord('a'))
+        transform = lambda x: math.exp(ord(x))
+        c = {}
+
+        for i in strs:
+            s = sum([transform(x) for x in i])
+            if (s, len(i)) in c:
+                c[(s, len(i))].append(i)
+            else:
+                c[(s, len(i))] = [i]
+
+        return list(c.values())
